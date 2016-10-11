a7ecf77eb169059ac2df5e704780e3f799bd79e0
atari-s <ncat@atari.fm>
2016-10-10 23:02:55 -0400

    accept connection

---
+++ accept.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+
+require 'socket'
+
+server = Socket.new(:INET, :STREAM)
+addr = Socket.pack_sockaddr_in(4481, '0.0.0.0')
+server.bind(addr)
+server.listen(Socket::SOMAXCONN)
+
+p server.accept
