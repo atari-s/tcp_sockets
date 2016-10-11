3d42a2eb92d7591401bbf3104274513ca6a0f239
atari-s <ncat@atari.fm>
2016-10-10 21:50:18 -0400

    begin sockets

---
+++ bind.rb
@@ -0,0 +1,10 @@
+require 'socket'
+
+# Create it
+socket = Socket.new(:INET, :STREAM)
+
+# Create a C struct to hold the address for listening
+addr = Socket.pack_sockaddr_in(4481, '0.0.0.0')
+
+# Bind to it
+socket.bind(addr)
---
+++ first_socket.rb
@@ -0,0 +1,6 @@
+require 'socket'
+
+# socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+socket = Socket.new(:INET6, :STREAM)
+
+
